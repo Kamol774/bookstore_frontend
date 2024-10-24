cc48f6b2d35c6c15d632bde6ad4c9bd5d3df5e31
Kamol774 <chust1222@gmail.com>
2024-10-24 22:42:59 +0900

    feat: rooting system created

--- src/index.tsx
+++ src/index.tsx
@@ -8,17 +8,24 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import './css/index.css';
 import theme from './app/MaterialTheme';
+import { Route, BrowserRouter as Router } from "react-router-dom";
+
 
 ReactDOM.render(  // 2ta argument: 1-react, 2-reactDOM instance 
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         < CssBaseline />
-        < App />
+        <Router>
+          < App />
+        </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root') //reactDOM instance
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
